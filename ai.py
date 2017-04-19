57a20566c5b2b8e35fb5ca6a7bb557ac4e2ce9df
spagovir <edgarlin2000@gmail.com>
2017-04-19 08:17:56 -0700

    Added the ability to display guesses. Created skeleton structure for ai main loop.

---
+++ ai.py
@@ -0,0 +1,10 @@
+
+#spaces: number of characters in phrase
+#question: function that allows this function to guess
+#by returning the list of positions in which the letter exists
+#when provided with the letter as an argument
+def play(spaces, question):
+  guessed = [] #letters guessed already
+  nknown = 0 #number of positions known
+  while nknown < spaces:
+    pass #to finish
